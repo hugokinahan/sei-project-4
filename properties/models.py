6aa7c550b5577f5674288765cb4beaaddb4fae08
Elsie Down <elspethdown@gmail.com>
2021-01-27 09:54:15 +0000

    added seeds elsie first day

--- properties/models.py
+++ properties/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Property(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=50, unique=True)
     address = models.CharField(max_length=60)
     city = models.CharField(max_length=30)
     country = models.CharField(max_length=30)
     continent = models.CharField(max_length=30)
-    description = models.CharField(max_length=200)
-    property_image = models.CharField(max_length=300)
+    description = models.CharField(max_length=600)
+    property_image = models.CharField(max_length=500)
     is_available = models.BooleanField(default=False)
-    latitude = models.IntegerField()
-    longitude = models.IntegerField()
+    longitude = models.DecimalField(max_digits=30, decimal_places=20)
+    latitude = models.DecimalField(max_digits=30, decimal_places=20)
   
     def __str__(self):
       return f"{self.name} - {self.city}"
