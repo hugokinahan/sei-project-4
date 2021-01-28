7851724cb29411410b6a67c970f6aaad7a662115
Hugo Kinahan <hugokinahan@hotmail.com>
2021-01-28 19:58:40 +0000

    api functions added

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import { getToken } from './auth'
+
+function headers() {
+  return {
+    headers: { Authorization: `Bearer ${getToken()}` }
+  }
+}
+
 const baseUrl = '/api'
 
-// Get Properties Requests
+
+// * Get Properties Requests
 
 // Get All Properties
 
@@ -13,4 +22,78 @@ export function getAllProperties() {
 
 export function getSingleProperty(id) {
   return axios.get(`${baseUrl}/properties/${id}`)
+}
+
+// Create a Property
+
+export function createProperty(formdata) {
+  return axios.post(`${baseUrl}/properties`, formdata, headers())
+}
+
+// Delete a property 
+
+export function deleteProperty(id) {
+  return axios.delete(`${baseUrl}/properties/${id}`, headers())
+}
+
+// Edit a Property
+
+export function editProperty(id, formdata) {
+  return axios.put(`${baseUrl}/properties/${id}`, formdata, headers())
+}
+
+// * Reviews requests
+
+// Get All Reviews
+
+export function getAllReviews() {
+  return axios.get(`${baseUrl}/reviews`)
+}
+
+// Get One Review
+
+export function getSingleReview(id) {
+  return axios.get(`${baseUrl}/reviews/${id}`)
+}
+
+// Create a Property Review
+
+export function createPropertyReview(formdata, id) {
+  return axios.post(`${baseUrl}/properties/${id}/reviews`, formdata, headers())
+}
+
+// Create a User Review
+
+export function createUserReview(formdata, id) {
+  return axios.post(`${baseUrl}/profile/${id}/reviews`, formdata, headers())
+}
+
+// Delete Property Review
+
+export function deletePropertyReview(id, reviewId) {
+  return axios.delete(`${baseUrl}/properties/${id}/reviews/${reviewId}`, headers())
+}
+
+// Delete User Review
+
+export function deleteUserReviw(id, reviewId) {
+  return axios.delete(`${baseUrl}/profile/${id}/reviews/${reviewId}`, headers())
+}
+
+// * Auth Requests
+
+export function registerUser(formdata) {
+  return axios.post(`${baseUrl}/register`, formdata)
+}
+
+export function loginUser(formdata) {
+  return axios.post(`${baseUrl}/login`, formdata)
+}
+
+export function showUserProfile() {
+  return axios.get(`${baseUrl}/profile/`, headers())
+}
+
+export function editUserProfile(formdata) {
+  return axios.put(`${baseUrl}/update-profile/`, formdata, headers())
 }
